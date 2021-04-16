58f27dcf85c7f103063ee5b2aa7f4137dec5f9d0
Yanik Sweeney <yanikisnice@gmail.com>
2021-04-16 08:29:24 -0400

    AJOUT routes et controleurs pour le projet

--- app/Models/Bateau.php
+++ app/Models/Bateau.php
@@ -5,16 +5,28 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Model représentant un Bateau dans la base de données
+ * @author Yanik Sweeney
+ */
 class Bateau extends Model
 {
     use HasFactory;
     protected $table = 'bateaux';
 
+    /**
+     * Illustre la relation un à plusieurs vers bateau_coordonnees
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
     public function bateau_coordonnees()
     {
         return $this->hasMany('App\Models\BateauCoordonnee');
     }
 
+    /**
+     * Renvoit un array digeste de coordonnees sous le format Rangee-Colonne (ex: A-1, B-2, etc.)
+     * @return array
+     */
     public function coordonnees()
     {
         $coords = [];
